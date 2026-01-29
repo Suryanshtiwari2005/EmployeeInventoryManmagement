66780205aaf779923dd46ba1ece54786119a84c9
Suryansh Tiwari <suryansh2005tiwari@gmail.com>
2026-01-29 14:09:48 +0530

    Restore project files after cleanup

---
+++ src/main/java/com/inventoryEmployee/demo/InventoryEmployeeApplication.java
@@ -0,0 +1,14 @@
+package com.inventoryEmployee.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryEmployeeApplication.class, args);
+		System.out.println("Hello Baby");
+	}
+
+}
---
+++ src/test/java/com/inventoryEmployee/demo/InventoryEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventoryEmployee.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
